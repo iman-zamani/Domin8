969434b83409eec134fb2678c149965c8ab1e5aa
iman zamani <izamanimoghaddam@gmail.com>
2026-02-15 22:08:44 +0330

    start refactoring

--- src/board.h
+++ src/board.h
@@ -1,18 +1,24 @@
 #pragma once 
-typedef struct Board{
-    //         [row][column]
-    int squares[8][8] ;
-    bool whiteShortCastle;
-    bool whiteLongCastle;
-    bool blackShortCastle;
-    bool blackLongCastle;
-    bool isWhiteTurn;
-    int targetEnPassantSquare;
-    int halfMovesFromLastCaptureOrPawnMove;
-    int numFullMoves;
-}Board;
-
-void readFenIntoBoard(Board * board ,char * FEN);
-void printBoard(Board *board);
-bool boardIsCheck(Board *board);
-bool applyMove(Move move);
+#include <stdbool.h>
+#include <stdint.h>
+#include "move.h"
+struct {
+    bool is_white_turn, white_king_side_castle, white_queen_side_castle, black_king_side_castle, black_queen_side_castle;
+    move* previous_move;
+    void* previous_board;
+    // bit maps
+    // white
+    uint64_t white_rooks_bm;
+    uint64_t white_bishops_bm;
+    uint64_t white_knights_bm;
+    uint64_t white_queens_bm;
+    uint64_t white_king_bm;
+    uint64_t white_pawns_bm;
+    // black
+    uint64_t black_rooks_bm;
+    uint64_t black_bishops_bm;
+    uint64_t black_knights_bm;
+    uint64_t black_queens_bm;
+    uint64_t black_king_bm;
+    uint64_t black_pawns_bm;
+}typedef Board;
--- src/move.h
+++ src/move.h
@@ -1,15 +1,20 @@
 #pragma once 
-typedef struct Move{
-    int pieceType;
-    int startSquare;
-    int targetSquare;
-    bool isCapture;
-    bool enPassant; // yet to be added 
-    bool pawnMovingTwoSquares;
-    bool shortCastle;
-    bool longCastle;
-    int promotionPieceType;
-}Move;
-
-
-Move createTempMove(int tPieceType, int tStartSquare, int tTargetSquare, bool tIsCapture);
+// while pieces 
+#define WHITE_PAWN 1
+#define WHITE_BISHOP 4
+#define WHITE_KNIGHT 3
+#define WHITE_KING 20
+#define WHITE_QUEEN 10
+#define WHITE_ROOK 5
+// black pieces
+#define BLACK_PAWN -1
+#define BLACK_BISHOP -4
+#define BLACK_KNIGHT -3
+#define BLACK_KING -20
+#define BLACK_QUEEN -10
+#define BLACK_ROOK -5
+struct {
+    int start_square;
+    int dest_square;
+    int piece_type;
+}typedef move;
